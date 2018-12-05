4c2e6ef20fc327f52d4f85e29040d8315d698824
Wei Huang <wei.huang1@ibm.com>
2018-12-04 16:45:55 -0800

    iks demo

---
+++ demos/iks/demo.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+source $(dirname "$0")/../demoscript
+source ~/.iks
+
+comment This is a demo on IBM Kubernetes Service
+
+doit source ~/.iks
+doit echo "\$KUBECONFIG"
+
+doit kubectl get node
+doit kubectl version --short
+
+doit kubectl get svc
+doit kubectl get pod
+
+comment Create 2 deployments - one TCP and one UDP
+
+doit cat demos/iks/deployments/*
+doit kubectl create -f demos/iks/deployments/
+doit kubectl get pod
+
+comment Custom Resource Definition has been pre-created
+doit kubectl get crd
+doit kubectl get slb
+
+comment We are going to create 2 SharedLB CRs
+doit cat demos/iks/crs/cr-tcp.yaml
+doit cat demos/iks/crs/cr-udp.yaml
+
+doit kubectl create -f demos/iks/crs/cr-tcp.yaml
+doit kubectl get svc
+doit kubectl get slb
+
+doit kubectl create -f demos/iks/crs/cr-udp.yaml
+doit kubectl get svc
+doit kubectl get slb
+
+comment Use nc to try connecting
+
+doit "echo 'tcp-test $(date)'  | nc $(kubectl get slb | grep sharedlb-tcp | awk '{print $2}') $(kubectl get slb sharedlb-tcp | grep sharedlb-tcp | awk '{print $3}')"
+doit "echo 'udp-test $(date)' | nc -w 1 -u $(kubectl get slb | grep sharedlb-udp | awk '{print $2}') $(kubectl get slb sharedlb-udp | grep sharedlb-udp | awk '{print $3}')"
+
+comment Checkout pod logs
+
+doit kubectl logs $(kubectl get po | grep tcp-deploy | awk '{print $1}')
+doit kubectl logs $(kubectl get po | grep udp-deploy | awk '{print $1}')
+
+# comment Cleanup
+
+# doit kubectl delete deploy --all
+# doit kubectl delete slb --all
+# doit kubectl delete svc -l=lb-template=
+
+comment ~End~
